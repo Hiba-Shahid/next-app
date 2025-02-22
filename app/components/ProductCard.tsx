4ade07718e095716caafc0765d2aea2539ecdedc
Hiba-Shahid <hibashahid678@gmail.com>
2025-02-22 13:11:15 +0500

    Add user pages and product card component; update webpack cache and manifest

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div><button onClick={()=> console.log('Click')}>Add To Cart</button></div>
+  )
+}
+
+export default ProductCard
